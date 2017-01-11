b2d2231e3e2b56c732c01d5c36fa0f2beb7f878a
MrEvill <ppiskorz488@gmail.com>
2017-01-11 07:54:33 +0100

    Add files via upload

---
+++ NaMiejscu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cukiernia_WPF
+{
+    //Tworze klase i ustawiam klasę nadrzędną Zamówienie(dziedziczenie)
+    class NaMiejscu : Zamowienie
+    {
+    }
+}
---
+++ Zamowienie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cukiernia_WPF
+{
+    //Dodaje klasę Abstrakcyjną
+   abstract class Zamowienie
+    {
+        protected DateTime czasDostawy;
+
+        //dodaje virtualna metode
+        public virtual bool PoprawnyCzas()
+        {
+            if (czasDostawy > DateTime.Now)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //ktora ustawia parametr jako pole czasDostawy?!?!----------------------
+        public void UstawCzasDostawy (DateTime CzasDostawy)
+        {
+            this.czasDostawy = CzasDostawy;
+        }
+    }
+}
